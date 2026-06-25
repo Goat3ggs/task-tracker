66da4fda7eb4bcbfef28943e252814798b943341
Stefania <stefpandi12@gmail.com>
2026-06-25 20:43:42 +0300

    feat: create edit task functionality

--- script.js
+++ script.js
@@ -50,7 +50,9 @@ const renderTask = (task) => {
         >
         <div class="checkmark"></div>
       </label>
-      <button class="delete-btn" data-task-id="${task.id}">  <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg></button>
+      <button class="task-button button--delete" data-task-id="${task.id}">  <svg viewBox="0 0 448 512" class="svgIcon--delete"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg></button>
+
+      <button class="task-button button--edit" data-task-id="${task.id}"><img class="svgIcon--edit" src="./edit-button.svg"></button>
     </div>
   `;
 
@@ -75,7 +77,7 @@ form.addEventListener("submit", (event) => {
 });
 
 const deleteTask = (event) => {
-  const deleteBtn = event.target.closest("button[data-task-id]");
+  const deleteBtn = event.target.closest(".button--delete[data-task-id]");
 
   if (!deleteBtn) return;
 
@@ -91,6 +93,30 @@ const deleteTask = (event) => {
     element.remove();
     taskElements.delete(taskId);
   }
+};
+
+const editTask = (event) => {
+  const editBtn = event.target.closest(".button--edit[data-task-id]");
+
+  if (!editBtn) return;
+
+  const taskId = Number(editBtn.dataset.taskId);
+  const task = tasks.find((task) => task.id === taskId);
+
+  if (!task) return;
+
+  const newTitle = prompt("Edit this text", task.title);
+
+  if (newTitle === null) return;
+
+  const cleanTitle = newTitle.trim();
+
+  if (!cleanTitle) return;
+
+  task.title = cleanTitle;
+
+  saveTasks();
+  renderTasks(tasks);
 };
 
 const toggleComplete = (event) => {
@@ -150,6 +176,7 @@ filtersContainer.addEventListener("click", (event) => {
 
 taskList.addEventListener("click", (event) => {
   deleteTask(event);
+  editTask(event);
   toggleComplete(event);
 });
 
